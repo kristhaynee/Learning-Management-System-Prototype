a202155c9f5e0c0a2ce708cc84102909801e28b8
RoviK31 <rovikeith.navarro.y@bulsu.edu.ph>
2023-09-14 09:48:37 +0800

    add scroll animation in homepage

--- client/assets/js/scroll.js
+++ client/assets/js/scroll.js
@@ -2,9 +2,13 @@ const observer = new IntersectionObserver(entries=>{
     entries.forEach(entry=>{
         if(entry.isIntersecting){
             entry.target.classList.add('scroll-show')
+            entry.target.classList.add('scroll-slide-show')
         }
     })
 })
 
 const scrollHidden = document.querySelectorAll('.scroll-hidden')
+const scrollSlideHidden = document.querySelectorAll('.scroll-slide-hidden')
+
 scrollHidden.forEach(el=> observer.observe(el))
+scrollSlideHidden.forEach(el=> observer.observe(el))
