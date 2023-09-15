03c1d22587982db78a7e01c362958a0fdc496a2a
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2023-09-15 10:31:16 +0800

    Update users.php

--- client/back-office/users.php
+++ client/back-office/users.php
@@ -21,109 +21,109 @@ include('includes/script.php');
     <!-- TABLE STUDENT -->
     <div class="container mt-3 table-responsive tab-pane fade show active" role="tabpanel" aria-labelledby="nav-student-tab" id="nav-student">
         <div class="card">
-            <div class="card-body">
-            <table id="example1" class="table pt-3 px-5" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>Student ID</th>
-                        <th>Full Name</th>
-                        <th>Email</th>
-                        <th>Username</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>stud123</td>
-                        <td>Tiger Nixon</td>
-                        <td>john_fernando@gmail.com</td>
-                        <td>j_anthony</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>stud124</td>
-                        <td>Krizzia Marie Cruz</td>
-                        <td>k_marie@gmail.com</td>
-                        <td>k_marie</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>stud125</td>
-                        <td>Alfredo Marquez</td>
-                        <td>alfredo_marq@gmail.com</td>
-                        <td>alfmarq</td>
-                        <td>inactive</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>stud126</td>
-                        <td>Andrea Cordoje</td>
-                        <td>andy_C@gmail.com</td>
-                        <td>andyy</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>stud127</td>
-                        <td>Martin Loui Cruz</td>
-                        <td>martinL_cruz@gmail.com</td>
-                        <td>martinL</td>
-                        <td>inactive</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>stud128</td>
-                        <td>Missy Angela Carlos</td>
-                        <td>missyAC@gmail.com</td>
-                        <td>missyAC</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>stud129</td>
-                        <td>Herrod Chandler</td>
-                        <td>herrod_c@gmail.com</td>
-                        <td>herrodC</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>stud130</td>
-                        <td>Rhona Davidson</td>
-                        <td>rhona_davidson@gmail.com</td>
-                        <td>rhona</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>stud131</td>
-                        <td>Colleen Hurst</td>
-                        <td>ColleenHurst@gmail.com</td>
-                        <td>Colleen</td>
-                        <td>inactive</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>stud132</td>
-                        <td>Hermione Butler</td>
-                        <td>Butler_Hermione@gmail.com</td>
-                        <td>Hermioneee</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>stud133</td>
-                        <td>Lael Greer</td>
-                        <td>LaelGreer@gmail.com</td>
-                        <td>LaelG</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="card-body m-5 pt-4 px-3">
+                <table id="example1" class="table pt-3 px-5" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>Student ID</th>
+                            <th>Full Name</th>
+                            <th>Email</th>
+                            <th>Username</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>stud123</td>
+                            <td>Tiger Nixon</td>
+                            <td>john_fernando@gmail.com</td>
+                            <td>j_anthony</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>stud124</td>
+                            <td>Krizzia Marie Cruz</td>
+                            <td>k_marie@gmail.com</td>
+                            <td>k_marie</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>stud125</td>
+                            <td>Alfredo Marquez</td>
+                            <td>alfredo_marq@gmail.com</td>
+                            <td>alfmarq</td>
+                            <td>inactive</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>stud126</td>
+                            <td>Andrea Cordoje</td>
+                            <td>andy_C@gmail.com</td>
+                            <td>andyy</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>stud127</td>
+                            <td>Martin Loui Cruz</td>
+                            <td>martinL_cruz@gmail.com</td>
+                            <td>martinL</td>
+                            <td>inactive</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>stud128</td>
+                            <td>Missy Angela Carlos</td>
+                            <td>missyAC@gmail.com</td>
+                            <td>missyAC</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>stud129</td>
+                            <td>Herrod Chandler</td>
+                            <td>herrod_c@gmail.com</td>
+                            <td>herrodC</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>stud130</td>
+                            <td>Rhona Davidson</td>
+                            <td>rhona_davidson@gmail.com</td>
+                            <td>rhona</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>stud131</td>
+                            <td>Colleen Hurst</td>
+                            <td>ColleenHurst@gmail.com</td>
+                            <td>Colleen</td>
+                            <td>inactive</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>stud132</td>
+                            <td>Hermione Butler</td>
+                            <td>Butler_Hermione@gmail.com</td>
+                            <td>Hermioneee</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>stud133</td>
+                            <td>Lael Greer</td>
+                            <td>LaelGreer@gmail.com</td>
+                            <td>LaelG</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
@@ -131,109 +131,118 @@ include('includes/script.php');
     <!-- TABLE TEACHER -->
     <div class="container mt-3 table-responsive tab-pane fade" role="tabpanel" aria-labelledby="nav-teacher-tab" id="nav-teacher">
         <div class="card">
-            <div class="card-body">
-            <table id="example2" class="table pt-3 px-5" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>Teacher ID</th>
-                        <th>Full Name</th>
-                        <th>Email</th>
-                        <th>Username</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>teach001</td>
-                        <td>Tiger Nixon</td>
-                        <td>john_fernando@gmail.com</td>
-                        <td>j_anthony</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>teach002</td>
-                        <td>Krizzia Marie Cruz</td>
-                        <td>k_marie@gmail.com</td>
-                        <td>k_marie</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>teach003</td>
-                        <td>Alfredo Marquez</td>
-                        <td>alfredo_marq@gmail.com</td>
-                        <td>alfmarq</td>
-                        <td>inactive</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>teach004</td>
-                        <td>Andrea Cordoje</td>
-                        <td>andy_C@gmail.com</td>
-                        <td>andyy</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>teach005</td>
-                        <td>Martin Loui Cruz</td>
-                        <td>martinL_cruz@gmail.com</td>
-                        <td>martinL</td>
-                        <td>inactive</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>teach006</td>
-                        <td>Missy Angela Carlos</td>
-                        <td>missyAC@gmail.com</td>
-                        <td>missyAC</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>teach007</td>
-                        <td>Herrod Chandler</td>
-                        <td>herrod_c@gmail.com</td>
-                        <td>herrodC</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>teach008</td>
-                        <td>Rhona Davidson</td>
-                        <td>rhona_davidson@gmail.com</td>
-                        <td>rhona</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>teach009</td>
-                        <td>Colleen Hurst</td>
-                        <td>ColleenHurst@gmail.com</td>
-                        <td>Colleen</td>
-                        <td>inactive</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>teach010</td>
-                        <td>Hermione Butler</td>
-                        <td>Butler_Hermione@gmail.com</td>
-                        <td>Hermioneee</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>teach011</td>
-                        <td>Lael Greer</td>
-                        <td>LaelGreer@gmail.com</td>
-                        <td>LaelG</td>
-                        <td>active</td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
+
+            <div class="row">
+                <div class="col-md-12 pt-4">
+                    <div class="new-user float-sm-end">
+                        <a href="users.php" class="new-user-form">Add New Teacher</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body mx-5  px-3">
+                <table id="example2" class="table table pt-3 px-5" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>Teacher ID</th>
+                            <th>Full Name</th>
+                            <th>Email</th>
+                            <th>Username</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>teach001</td>
+                            <td>Tiger Nixon</td>
+                            <td>john_fernando@gmail.com</td>
+                            <td>j_anthony</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>teach002</td>
+                            <td>Krizzia Marie Cruz</td>
+                            <td>k_marie@gmail.com</td>
+                            <td>k_marie</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>teach003</td>
+                            <td>Alfredo Marquez</td>
+                            <td>alfredo_marq@gmail.com</td>
+                            <td>alfmarq</td>
+                            <td>inactive</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>teach004</td>
+                            <td>Andrea Cordoje</td>
+                            <td>andy_C@gmail.com</td>
+                            <td>andyy</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>teach005</td>
+                            <td>Martin Loui Cruz</td>
+                            <td>martinL_cruz@gmail.com</td>
+                            <td>martinL</td>
+                            <td>inactive</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>teach006</td>
+                            <td>Missy Angela Carlos</td>
+                            <td>missyAC@gmail.com</td>
+                            <td>missyAC</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>teach007</td>
+                            <td>Herrod Chandler</td>
+                            <td>herrod_c@gmail.com</td>
+                            <td>herrodC</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>teach008</td>
+                            <td>Rhona Davidson</td>
+                            <td>rhona_davidson@gmail.com</td>
+                            <td>rhona</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>teach009</td>
+                            <td>Colleen Hurst</td>
+                            <td>ColleenHurst@gmail.com</td>
+                            <td>Colleen</td>
+                            <td>inactive</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>teach010</td>
+                            <td>Hermione Butler</td>
+                            <td>Butler_Hermione@gmail.com</td>
+                            <td>Hermioneee</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>teach011</td>
+                            <td>Lael Greer</td>
+                            <td>LaelGreer@gmail.com</td>
+                            <td>LaelG</td>
+                            <td>active</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
