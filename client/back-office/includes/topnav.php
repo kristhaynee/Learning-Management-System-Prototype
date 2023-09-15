6fd4176abf459d09d76ab590d40dcf8e5a652eab
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2023-09-15 08:13:05 +0800

    Create topnav.php

---
+++ client/back-office/includes/topnav.php
@@ -0,0 +1,67 @@
+<body>
+    <header>
+    <!-- LOGO SECTION -->
+    <div class="logo-section pt-3 pb-3 border-bottom border-2">
+        <nav class="navbar navbar-expand-sm">
+            <div class="container">
+                <a class="navbar-brand" href="Homepage.php"><img class="logo-pisa" src="../assets/images/Logo_COLORED.png" alt=""></a>
+            </div>
+        </nav>
+    </div>
+
+    <!-- Navigation Bar -->
+    <div class="d-flex justify-content-center pt-3 pb-3 top-nav navbar-section" id="navbar-section">
+        <nav class="navbar navbar-expand-sm">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ms-auto mb-1 mb-lg-0">
+
+                <li class="nav-item mx-5">
+                    <div class="col text-center">
+                        <div class="d-flex flex-column align-items-center">
+                        <i class="fa-solid fa-table-columns"></i>
+                        <a class="nav-link active px-3" aria-current="page" href="dashboard.php">Dashboard</a>
+                        </div>
+                    </div>
+                </li>
+
+                <li class="nav-item mx-5">
+                    <div class="col text-center">
+                        <div class="d-flex flex-column align-items-center">
+                        <i class="fa-solid fa-user"></i>
+                        <a class="nav-link px-3" href="users.php">Users</a>
+                        </div>
+                    </div>
+                </li>
+
+                <li class="nav-item mx-5">
+                    <div class="col text-center">
+                        <div class="d-flex flex-column align-items-center">
+                        <i class="fa-regular fa-lightbulb"></i>
+                        <a class="nav-link px-3" href="AboutUs.php">Assessments</a>
+                        </div>
+                    </div>
+                </li>
+
+                <li class="nav-item mx-5">
+                    <div class="col text-center">
+                        <div class="d-flex flex-column align-items-center">
+                        <i class="fa-solid fa-book-open"></i>
+                        <a class="nav-link px-3" href="AboutUs.php">Subject</a>
+                        </div>
+                    </div>
+                </li>
+
+                <li class="nav-item mx-5">
+                    <div class="col text-center">
+                        <div class="d-flex flex-column align-items-center">
+                        <i class="fa-solid fa-file-invoice"></i>
+                        <a class="nav-link px-3" href="AboutUs.php">Reports</a>
+                        </div>
+                    </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+    </div>
+    </header>
