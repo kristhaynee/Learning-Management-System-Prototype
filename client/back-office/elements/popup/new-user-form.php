681bb83744f027f2349f1800ffff59ce4427bf05
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2023-09-15 10:31:10 +0800

    Create new-user-form.php

---
+++ client/back-office/elements/popup/new-user-form.php
@@ -0,0 +1,8 @@
+<!-- popup.php -->
+<div id="popup" class="popup">
+    <!-- Your pop-up card content goes here -->
+    <div class="popup-content">
+        <h2>Pop-Up Card</h2>
+        <p>This is your pop-up content.</p>
+    </div>
+</div>
