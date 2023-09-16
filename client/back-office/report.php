148cd113d4289524b065522ee6e0d1aef0d2a61c
RoviK31 <rovikeith.navarro.y@bulsu.edu.ph>
2023-09-16 12:34:18 +0800

    report page

---
+++ client/back-office/report.php
@@ -0,0 +1,105 @@
+<?php
+include('includes/header.php');
+include('includes/topnav.php');
+include('includes/script.php');
+?>
+
+
+<section class="content">
+
+    <div class="container mt-1 mb-1">
+       
+
+            <div class="mb-5 col-8">
+                <p class="page-name fw-bold"><span class="border-bottom  border-warning">Stud</span>ent Reports</p>
+            </div>
+
+        
+    </div>
+
+    <!-- TABLE STUDENT -->
+    <div class="container mt-3 table-responsive tab-pane fade show active" role="tabpanel" aria-labelledby="nav-student-tab" id="nav-student">
+        <div class="card">
+            <div class="card-body m-5 pt-4 px-3">
+                <table id="example1" class="table pt-3 px-5" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>Student ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Section</th>
+                            <th>Final Grade</th>
+                            <th class="text-center">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>stud123</td>
+                            <td>Tiger Nixon</td>
+                            <td>john_fernando@gmail.com</td>
+                            <td>4M-G1</td>
+                            <td>1.50</td>
+                            <td ><button class="btn text-primary">View Details</button></td>
+                        </tr>
+                        <tr>
+                            <td>stud124</td>
+                            <td>Krizzia Marie Cruz</td>
+                            <td>k_marie@gmail.com</td>
+                            <td>4M-G1</td>
+                            <td>1.25</td>
+                             <td ><button class="btn text-primary">View Details</button></td>
+                        </tr>
+                        <tr>
+                            <td>stud125</td>
+                            <td>Andrea Cordoje</td>
+                            <td>andy_c@gmail.com</td>
+                            <td>4M-G1</td>
+                            <td>1.75</td>
+                             <td ><button class="btn text-primary">View Details</button></td>
+                        </tr>
+                        <tr>
+                            <td>stud126</td>
+                            <td>Martin Loui Cruz</td>
+                            <td>martinL_cruz@gmail.com</td>
+                            <td>4M-G2</td>
+                            <td>1</td>
+                             <td ><button class="btn text-primary">View Details</button></td>
+                        </tr>
+                        <tr>
+                            <td>stud127</td>
+                            <td>Missy Angela Carlos</td>
+                            <td>missyAC@gmail.com</td>
+                            <td>4M-G2</td>
+                            <td>1.2</td>
+                             <td ><button class="btn text-primary">View Details</button></td>
+                        </tr>
+                        <tr>
+                            <td>stud128</td>
+                            <td>Raven Quambao</td>
+                            <td>ravenQ@gmailcom</td>
+                            <td>4M-G2</td>
+                            <td>1.75</td>
+                             <td ><button class="btn text-primary">View Details</button></td>
+                        </tr>
+                        <tr>
+                            <td>stud129</td>
+                            <td>Luidel Garcia</td>
+                            <td>Luidel@gmail.com</td>
+                            <td>4M-G2</td>
+                            <td>2.0</td>
+                             <td ><button class="btn text-primary">View Details</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <!-- TABLE TEACHER -->
+
+
+    <!-- TABLE -->
+</section>
+
+</body>
+</html>
