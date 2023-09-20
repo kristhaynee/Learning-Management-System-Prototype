3c4f86b35390fff39ff43a54837cf687301456b6
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2023-09-20 10:45:01 +0800

    Update header.php

--- client/back-office/includes/header.php
+++ client/back-office/includes/header.php
@@ -15,6 +15,8 @@
     />
     <link rel="stylesheet" href="../assets/css/dashboard_styles.css" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
 
 
     <!-- Fonts and icons -->
