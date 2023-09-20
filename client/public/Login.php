eacb57ec7cbb6efe2fbb849a0c0a06c3e076fa79
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2023-09-20 10:43:38 +0800

    Update Login.php

--- client/public/Login.php
+++ client/public/Login.php
@@ -14,10 +14,34 @@ session_start();
       integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
       crossorigin="anonymous"
     />
+    <script src="https://kit.fontawesome.com/bbd71fca16.js" crossorigin="anonymous"></script>
+    <link href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" rel="stylesheet">
+
     <link rel="stylesheet" href="../assets/css/styles.css" />
     <link rel="stylesheet" href="../assets/css/scroll.css">
     <script defer src="../assets/js/scroll.js"></script>
   </head>
+
+  <style>
+    div.input-group-addon{
+      padding: 0.5rem !important;
+      display: flex !important;
+      align-items: center !important;
+      border-top: 1px solid #DEE2E6;    /* Add border to the top */
+      border-right: 1px solid #DEE2E6;  /* Add border to the right */
+      border-bottom: 1px solid #DEE2E6; /* Add border to the bottom */
+      border-radius: 0 5px 5px 0;
+    }
+
+    .fa{
+      color: #24B1D6;
+      text-decoration: none;
+    }
+
+    .eye-link {
+      text-decoration: none; /* Remove underline */
+    }
+  </style>
 
   <body class="gradient-background">
     <nav class="navbar navbar-expand-sm marginTB">
@@ -94,17 +118,24 @@ session_start();
                 />
                 <label for="floatingInput">Enter your email address</label>
               </div>
-              <div class="form-floating mb-3">
-                <input
-                  type="password"
-                  class="form-control"
-                  id="floatingPassword"
-                  placeholder="Password"
-                  required
-                  name="password"
-                />
-                <label for="floatingPassword">Password</label>
-              </div>
+
+
+              <div class="col-sm-12">
+                <div class="input-group" id="show_hide_password">
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="passwordInput" placeholder="Password" name="password" required>
+                        <label for="passwordInput">Password <span style="color:red;">*</span></label>
+                    </div>
+                    <div class="input-group-addon">
+                        <a href="#" id="togglePassword" class="eye-link">
+                            <i class="fa fa-eye-slash" id="eyeSlashIcon" aria-hidden="true"></i>
+                            <i class="fa fa-eye" id="eyeIcon" aria-hidden="true" style="display: none;"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+
               <button class="d-grid gap-2 col-6 mx-auto btn text-white font-weight marginTB btn-secondary-color p-3 mt-5 mb-4"
               type="submit" name="login_btn" value="login">
               Log in
@@ -119,6 +150,24 @@ session_start();
     </section>
 
 
+    <script>
+    document.getElementById('togglePassword').addEventListener('click', function(e) {
+        e.preventDefault();
+        const passwordInput = document.getElementById('passwordInput');
+        const eyeIcon = document.getElementById('eyeIcon');
+        const eyeSlashIcon = document.getElementById('eyeSlashIcon');
+
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            eyeIcon.style.display = 'inline';
+            eyeSlashIcon.style.display = 'none';
+        } else {
+            passwordInput.type = 'password';
+            eyeIcon.style.display = 'none';
+            eyeSlashIcon.style.display = 'inline';
+        }
+    });
+    </script>
 
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
