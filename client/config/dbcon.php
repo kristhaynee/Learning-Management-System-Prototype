ad5dcc5e88f6f6622652a374324f794f5a039806
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2023-10-15 18:35:32 +0800

    Update dbcon.php

--- client/config/dbcon.php
+++ client/config/dbcon.php
@@ -3,14 +3,12 @@ $servername = "localhost";
 $username = "u741634386_PIproto";
 $password = "L9k9g#/#pb&";
 $database = "u741634386_lms_prototype";
-
+ 
 $con = mysqli_connect($servername, $username, $password, $database);
- 
+
 if (!$con) {
- 
-    die("Connection failed: " . mysqli_connect_error());
- 
+    die("Database connection failed: " . mysqli_connect_error());
+} else {
+    echo "Database connection successful.";
 }
-echo "Connected successfully";
-mysqli_close($conn);
 ?>
