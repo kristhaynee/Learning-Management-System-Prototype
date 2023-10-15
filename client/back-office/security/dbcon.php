1488d50ccd0cb87a7bf3906dcb2282c8e2ab78d3
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2023-10-15 21:21:29 +0800

    Update dbcon.php

--- client/back-office/security/dbcon.php
+++ client/back-office/security/dbcon.php
@@ -1,13 +1,14 @@
 <?php
 $host = "localhost";
-$username = "root";
-$password = "";
-$database = "lms_prototype";
-
+$username = "u741634386_PIproto";
+$password = "L9k9g#/#pb&";
+$database = "u741634386_lms_prototype";
+ 
 $con = mysqli_connect($host, $username, $password, $database);
 
 if (!$con) {
-    header ("Location: ../errors/dberror.php");
-    die();}
-
+    die("Database connection failed: " . mysqli_connect_error());
+} else {
+    echo "Database connection successful.";
+}
 ?>
