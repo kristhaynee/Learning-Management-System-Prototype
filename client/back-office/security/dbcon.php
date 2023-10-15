51fa1649fe55eb941005db2d79a35565e4e7a04b
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2023-10-15 22:05:55 +0800

    dbcon

--- client/back-office/security/dbcon.php
+++ client/back-office/security/dbcon.php
@@ -8,7 +8,5 @@ $con = mysqli_connect($host, $username, $password, $database);
 
 if (!$con) {
     die("Database connection failed: " . mysqli_connect_error());
-} else {
-    echo "Database connection successful.";
 }
 ?>
