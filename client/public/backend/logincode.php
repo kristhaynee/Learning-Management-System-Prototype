7f386c7572b6dd25ed17c402e210245222d61378
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2023-10-15 18:53:30 +0800

    Update logincode.php

--- client/public/backend/logincode.php
+++ client/public/backend/logincode.php
@@ -7,13 +7,16 @@ if(isset($_POST['login_btn']))
     $email = mysqli_real_escape_string($con, $_POST['email']);
     $password = mysqli_real_escape_string($con, $_POST['password']);
 
-    $login_query = "SELECT * FROM users WHERE BINARY email='$email' AND BINARY password='$password' LIMIT 1";
-    $login_query_run = mysqli_query($con, $login_query);
+    $login_query = "SELECT * FROM users WHERE BINARY email=? AND BINARY password=? LIMIT 1";
+    $stmt = mysqli_prepare($con, $login_query);
 
-    if(mysqli_num_rows($login_query_run)>0)
-    {
-        foreach($login_query_run as $data)
-    {
+    if ($stmt) {
+        mysqli_stmt_bind_param($stmt, "ss", $email, $password);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+
+        if ($data = mysqli_fetch_assoc($result))
+        {
             $user_id = $data['user_id'];
             $email = $data['email'];
             $role_as = $data['role_as'];
@@ -23,48 +26,53 @@ if(isset($_POST['login_btn']))
             $suffix = $data['suffix'];
             $username = $data['username'];
             $profile_img = $data['profile_img'];
-    }
 
-        $_SESSION['auth'] = true;
-        $_SESSION['auth_role'] = "$role_as"; // 0 = Admin, 1 = Teacher, 2 = Student
-        
-        $_SESSION['auth_user'] = [
-            'user_id' => $user_id,
-            'email' => $email,
-            'fname' => $fname,
-            'lname' => $lname,
-            'suffix' => $suffix,
-            'userCode' => $userCode,
-            'profile_img' => $profile_img,
-            'role_as' => $role_as,
-        ];
-        print_r($_SESSION['auth_user']);
+            $_SESSION['auth'] = true;
+            $_SESSION['auth_role'] = $role_as; // 0 = Admin, 1 = Teacher, 2 = Student
 
-        if($_SESSION['auth_role'] == '0') // Head Admin
+            $_SESSION['auth_user'] = [
+                'user_id' => $user_id,
+                'email' => $email,
+                'fname' => $fname,
+                'lname' => $lname,
+                'suffix' => $suffix,
+                'userCode' => $userCode,
+                'profile_img' => $profile_img,
+                'role_as' => $role_as,
+            ];
+
+            print_r($_SESSION['auth_user']);
+
+            if($role_as == '0') // Head Admin
+            {
+                $_SESSION['message'] = "Welcome to Admin dashboard $fname";
+                header("Location: ../../back-office/dashboard.php?user_id=" . $_SESSION['auth_user']['user_id']);
+                exit(0);
+            }
+            elseif($role_as == '1') // Teacher
+            {
+                $_SESSION['message'] = "Welcome to the dashboard $fname";
+                header("Location: ../../back-office/dashboard.php?user_id=" . $_SESSION['auth_user']['user_id']);
+                exit(0);
+            }
+            elseif($role_as == '2') // Student
+            {
+                header("Location: ../standby.php?user_id=" . $_SESSION['auth_user']['user_id']);
+                exit(0);
+            }
+        }
+        else
         {
-            $_SESSION['message'] = "Welcome to Admin dashboard"." ".$fname;
-            header("Location: ../../back-office/dashboard.php?user_id=" . $_SESSION['auth_user']['user_id']);
+            $_SESSION['message'] = "Invalid username or password";
+            header("Location: ../Login.php");
             exit(0);
         }
-        elseif($_SESSION['auth_role'] == '1') // Teacher
-        {
-            $_SESSION['message'] = "Welcome to the dashboard"." ".$fname;
-            header("Location: ../../back-office/dashboard.php?user_id=" . $_SESSION['auth_user']['user_id']);
-            exit(0);
-        }
-        elseif($_SESSION['auth_role'] == '2') // Student
-        {
-            header("Location: ../standby.php?user_id=" . $_SESSION['auth_user']['user_id']);
-            exit(0);
-        }
+
+        mysqli_stmt_close($stmt);
     }
     else
     {
-        $_SESSION['message'] = "Invalid username or password";
-        header("Location: ../Login.php");
-        exit(0);
+        echo "Error in the prepared statement.";
     }
 }
-
-
 ?>
